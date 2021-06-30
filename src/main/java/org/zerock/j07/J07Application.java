c65022dfd6b2b15c43af49a6cc469353072e0f71
khjkhj2006 <66585730+khjkhj2006@users.noreply.github.com>
2021-06-30 23:49:22 +0900

    Initial commit

---
+++ src/main/java/org/zerock/j07/J07Application.java
@@ -0,0 +1,13 @@
+package org.zerock.j07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J07Application.class, args);
+    }
+
+}
